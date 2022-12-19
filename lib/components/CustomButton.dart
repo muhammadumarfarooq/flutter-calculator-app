48fb96f3b68cf7b064f441270ae920ec91129490
umar <develop.umar@gmail.com>
2022-12-19 08:37:25 +0500

    collect button press values

--- lib/components/CustomButton.dart
+++ lib/components/CustomButton.dart
@@ -3,24 +3,31 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   final String title;
   final Color bgColor;
+  final VoidCallback onPress;
 
   const CustomButton(
-      {Key? key, required this.title, this.bgColor = const Color(0xff929193)})
+      {Key? key,
+      required this.title,
+      this.bgColor = const Color(0xff929193),
+      required this.onPress})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 80,
-        width: 80,
-        decoration: BoxDecoration(
-            color: bgColor, borderRadius: BorderRadius.circular(50)),
-        child: Center(
-          child: Text(
-            title,
-            style: TextStyle(fontSize: 18, color: Colors.white),
+      padding: const EdgeInsets.all(16),
+      child: InkWell(
+        onTap: onPress,
+        child: Container(
+          height: 80,
+          width: 80,
+          decoration: BoxDecoration(
+              color: bgColor, borderRadius: BorderRadius.circular(50)),
+          child: Center(
+            child: Text(
+              title,
+              style: const TextStyle(fontSize: 18, color: Colors.white),
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Calculator',
       theme:
-          ThemeData(primarySwatch: Colors.blue, backgroundColor: Colors.black),
+      ThemeData(primarySwatch: Colors.blue, backgroundColor: Colors.black),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -30,66 +30,147 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var userInput = '';
+  var answer = '0';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-      child: Column(children: [
-        Expanded(child: Text('0')),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  CustomButton(title: 'AC'),
-                  CustomButton(title: '+/-'),
-                  CustomButton(title: '%'),
-                  CustomButton(title: '/', bgColor: Color(0xfffe8a01)),
+          child: Column(children: [
+            Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Text(
+                      userInput.toString(),
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                    Text(
+                      answer.toString(),
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                  ],
+                )),
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(
+                        title: 'AC',
+                        onPress: () {
+                          userInput = '';
+                          answer = '';
+                          setState(() {});
+                        },
+                      ),
+                      CustomButton(title: '+/-', onPress: () {}),
+                      CustomButton(title: '%', onPress: () {}),
+                      CustomButton(
+                          title: '/',
+                          bgColor: const Color(0xfffe8a01),
+                          onPress: () {}),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(title: '7', onPress: () {
+                        userInput += '7';
+                        setState(() {});
+                      }),
+                      CustomButton(title: '8', onPress: () {
+                        userInput += '8';
+                        setState(() {});
+                      }),
+                      CustomButton(title: '0', onPress: () {
+                        userInput += '0';
+                        setState(() {});
+                      }),
+                      CustomButton(
+                          title: 'x',
+                          bgColor: Color(0xfffe8a01),
+                          onPress: () {
+                            userInput += '*';
+                            setState(() {});
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(title: '4', onPress: () {
+                        userInput += '4';
+                        setState(() {});
+                      }),
+                      CustomButton(title: '5', onPress: () {
+                        userInput += '5';
+                        setState(() {});
+                      }),
+                      CustomButton(title: '6', onPress: () {
+                        userInput += '6';
+                        setState(() {});
+                      }),
+                      CustomButton(
+                          title: '-',
+                          bgColor: Color(0xfffe8a01),
+                          onPress: () {
+                            userInput += '-';
+                            setState(() {});
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(title: '1', onPress: () {
+                        userInput += '1';
+                        setState(() {});
+                      }),
+                      CustomButton(title: '2', onPress: () {
+                        userInput += '2';
+                        setState(() {});
+                      }),
+                      CustomButton(title: '3', onPress: () {
+                        userInput += '3';
+                        setState(() {});
+                      }),
+                      CustomButton(
+                          title: '+',
+                          bgColor: Color(0xfffe8a01),
+                          onPress: () {
+                            userInput += '+';
+                            setState(() {});
+                          }),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(title: '0', onPress: () {}),
+                      CustomButton(title: '.', onPress: () {}),
+                      CustomButton(title: 'DEL', onPress: () {}),
+                      CustomButton(
+                          title: '=',
+                          bgColor: Color(0xfffe8a01),
+                          onPress: equalPressed),
+                    ],
+                  ),
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  CustomButton(title: '7'),
-                  CustomButton(title: '8'),
-                  CustomButton(title: '0'),
-                  CustomButton(title: 'x', bgColor: Color(0xfffe8a01)),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  CustomButton(title: '4'),
-                  CustomButton(title: '5'),
-                  CustomButton(title: '6'),
-                  CustomButton(title: '-', bgColor: Color(0xfffe8a01)),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  CustomButton(title: '1'),
-                  CustomButton(title: '2'),
-                  CustomButton(title: '3'),
-                  CustomButton(title: '+', bgColor: Color(0xfffe8a01)),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  CustomButton(title: '0'),
-                  CustomButton(title: '.'),
-                  CustomButton(title: 'DEL'),
-                  CustomButton(title: '=', bgColor: Color(0xfffe8a01)),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ]),
-    ));
+            ),
+          ]),
+        ));
+  }
+
+
+  void equalPressed() {
+    print('Equal pressed...');
   }
 }
